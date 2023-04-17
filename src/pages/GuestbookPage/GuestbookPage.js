9798462e7a492dad55144b3884d46719ecbcf8fc
yunhee1 <alkrg007@naver.com>
2023-04-17 21:59:12 +0900

    FEAT: 방명록 페이지 구성 및 배치, 메인페이지와 연결

---
+++ src/pages/GuestbookPage/GuestbookPage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../../css/guestbook-page.scss";
+import { ReactComponent as PinkMemo } from "../../images/GuestbookPage/PinkMemo.svg";
+import { ReactComponent as BrownMemo } from "../../images/GuestbookPage/BrownMemo.svg";
+import { ReactComponent as GreenMemo } from "../../images/GuestbookPage/GreenMemo.svg";
+import { ReactComponent as YellowMemo } from "../../images/GuestbookPage/YellowMemo.svg";
+
+const GuestbookPage = () => {
+  let visitnum = 10;
+
+  return (
+    <div className="container">
+      <p> 지금까지 {visitnum} 발자국이 찍혔어요! </p>
+      <div className="content">
+        <div className="memo">
+          <PinkMemo />
+        </div>
+        
+        <div className="memo">
+          <BrownMemo />
+        </div>
+
+        <div className="memo">
+          <YellowMemo />
+        </div>
+
+        <div className="memo">
+          <GreenMemo />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GuestbookPage;
