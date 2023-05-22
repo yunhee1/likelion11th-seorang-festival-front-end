1571642417975babc3246e93546c6cb867a43e55
ChaeYubin <cyoobin@naver.com>
2023-05-22 10:05:01 +0900

    Design: 랜딩페이지 소개 문구, 폰트 수정

--- src/pages/MapPage/BoothDateData.js
+++ src/pages/MapPage/BoothDateData.js
@@ -1,7 +1,7 @@
 const booth_date_info = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7],
+  [1, 2, 3, 8, 9, 6, 7],
+  [1, 2, 3, 8, 9, 4, 5],
 ];
 
 export default booth_date_info;
