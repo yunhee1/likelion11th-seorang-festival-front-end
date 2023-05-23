98ace0eb08444234571b80095c5f8e860040a452
ChaeYubin <cyoobin@naver.com>
2023-05-23 19:06:33 +0900

    http

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 const setupProxy = () => {
+  console.log("proxy 파일 접근");
   createProxyMiddleware("api", {
     target: "http://43.201.176.26:8080/",
     changeOrigin: true,
