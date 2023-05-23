7dfbb8ce7fe2e7dd9311ec8fa04651d805e14592
ChaeYubin <cyoobin@naver.com>
2023-05-23 20:10:19 +0900

    baseURL 재설정

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const host =
-  window.location.hostname === "localhost"
-    ? "http://43.201.176.26:8080/api"
-    : "http://43.201.176.26:8080/api/:splat 200";
-
 const API = axios.create({
-  baseURL: host,
+  baseURL: "http://43.201.176.26:8080/api",
   headers: {
     "Access-Control-Allow-Origin": `http://localhost:3000/`,
   },
